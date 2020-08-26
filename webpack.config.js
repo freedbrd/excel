c55445ca430084691f3af8c006599d0d37af29ab
vtrikhleb <vtrikhleb@healthprecision.com>
2020-08-26 13:24:55 +0300

    add webpack loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -26,7 +25,29 @@ module.exports = {
       template: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.[hash].css'
-    })
+      filename: 'bundle.[hash].css',
+    }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
